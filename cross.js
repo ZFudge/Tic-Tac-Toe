8fd1a6462afc2b6104e5cb8b6d1e35a1a34fa0cf
Zach Fudge <zacheryfudge@gmail.com>
2018-02-11 22:25:01 -0700

    thicker line, quicker movement

--- cross.js
+++ cross.js
@@ -1,6 +1,6 @@
 const line = {
   ms: 10,
-  radius: 5,
+  radius: 6,
   canvas: document.getElementById("cross-canvas"),
   cross(r1,c1,r2,c2) {
     const x1 = (c1 === 0) ? 50 : (c1===1) ? 170 : 290;
@@ -14,8 +14,8 @@ const line = {
     this.context.arc(x1,y1, this.radius, 0, 2*Math.PI);
     this.context.fill();
     if (Math.abs(x1-x2) > 6 || Math.abs(y1-y2) > 6) {
-      (x1 < x2) ? x1+=2.5 : x1-=2.5;
-      (y1 < y2) ? y1+=2.5 : y1-=2.5;
+      (x1 < x2) ? x1+=3 : x1-=3;
+      (y1 < y2) ? y1+=3 : y1-=3;
       setTimeout(() => {
         this.finishLine(x1,y1,x2,y2);
       }, this.ms);
